0b20d84cb261718d6cd6e750d7c9c9acb31478b1
yanis <yanisabdulmadjid@gmail.com>
2025-05-05 12:38:19 +0200

    Ajout de la methode de reduction et test effectuer

--- src/Calculator.java
+++ src/Calculator.java
@@ -3,9 +3,16 @@ public class Calculator {
         return (a + b);
     }
 
+    public static int reduce(int a, int b) {
+        return (a - b);
+    }
+
     public static void main(String[] args) {
         System.out.println("Somme : " + add(3, 2));
+        System.out.println("Reduction : " + reduce(3, 2));
     }
+
+
 }
 
 
--- tests/CalculatorTest.java
+++ tests/CalculatorTest.java
@@ -6,4 +6,9 @@ public class CalculatorTest {
     public void testAddition() {
         assertEquals(5, Calculator.add(3, 2));
     }
+
+    @Test
+    public void testReduction() {
+        assertEquals(1, Calculator.reduce(3, 2));
+    }
 }
